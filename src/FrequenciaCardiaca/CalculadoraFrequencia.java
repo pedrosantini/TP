a191ac5db65fb77cf9ab49e61aa18d53ce2ca000
Pedro Santini <pedroantoniosantini@gmail.com>
2024-10-17 22:34:27 -0300

    Date e Calculadora combinadas

--- src/FrequenciaCardiaca/CalculadoraFrequencia.java
+++ src/FrequenciaCardiaca/CalculadoraFrequencia.java
@@ -3,27 +3,46 @@ import java.util.Scanner;
 
 public class CalculadoraFrequencia {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner sc1 = new Scanner(System.in);
         FrequenciaCardiaca p = new FrequenciaCardiaca("Suzano", "de Souza", 15, 3,2001);
         FrequenciaCardiaca p2 = new FrequenciaCardiaca();
 
-        System.out.println("Digite seu nome: ");
-        String n = sc.nextLine();
-        System.out.println("Digite seu sobrenome:");
-        String s = sc.nextLine();
+        System.out.print("Digite o dia de nascimento: ");
+        int d = sc1.nextInt();
+        System.out.print("Digite o mes de nascimento: ");
+        int m = sc1.nextInt();
+        System.out.print("Digite o ano de nascimento: ");
+        int a = sc1.nextInt();
+
+        sc1.nextLine();
+
+        System.out.print("Digite seu nome: ");
+        String n = sc1.nextLine();
+        System.out.print("Digite seu sobrenome: ");
+        String s = sc1.nextLine();
+       
+        p2.setNascimento(d, m, a);
         
-        int d = sc.nextInt();
-        int m = sc.nextInt();
-        int a = sc.nextInt();
+        p2.setNome(n);
+        p2.setSobrenome(s);
 
+        System.out.print("Dia atual: ");
+        int dAt = sc1.nextInt();
+        System.out.print("Mes atual: ");
+        int mAt = sc1.nextInt();
+        System.out.print("Ano atual: ");
+        int aAt = sc1.nextInt();
 
+        System.out.println(p.obterIdade(dAt, mAt, aAt));
+        System.out.println(p2.obterIdade(dAt, mAt, aAt));
 
+        System.out.printf("A frequencia max de %s %s eh %d\n",p.getNome(), p.getSobrenome(),p.obterFrequenciaMax(dAt, mAt, aAt));
+        System.out.printf("A frequencia max de %s %s eh %d\n",p2.getNome(), p2.getSobrenome(),p2.obterFrequenciaMax(dAt, mAt, aAt));
 
-        System.out.println(p.obterIdade());
-        System.out.println(p2.obterIdade());
-
-        sc.close();
-        
+        System.out.printf("A frequencia ideal de %s %s eh %.2f\n",p.getNome(), p.getSobrenome(), p.obterFrequenciaIdeal(p.obterFrequenciaMax(dAt, mAt, aAt)));
+        System.out.printf("A frequencia ideal de %s %s eh %.2f\n",p2.getNome(), p2.getSobrenome(), p2.obterFrequenciaIdeal(p2.obterFrequenciaMax(dAt, mAt, aAt)));
+ 
+        sc1.close();        
 
     }
 }
--- src/FrequenciaCardiaca/FrequenciaCardiaca.java
+++ src/FrequenciaCardiaca/FrequenciaCardiaca.java
@@ -1,7 +1,57 @@
 package FrequenciaCardiaca;
-import java.util.Scanner;
 
 public class FrequenciaCardiaca {
+
+    public class Date {
+        private int dia;
+        private int mes;
+        private int ano;
+    
+        public Date() {
+            this.dia = 1;
+            this.mes = 1;
+            this.ano = 1900;
+        }
+    
+        public Date(int d, int m, int a) {
+            this.dia = d;
+            this.mes = m;
+            this.ano = a;
+        }
+    
+        public void setDia(int dia) {
+            this.dia = dia;
+        }
+    
+        public void setMes(int mes) {
+            if ((mes <= 12) && (mes > 0))
+                this.mes = mes;
+            else
+                mes = 1;
+        }
+    
+        public void setAno(int ano) {
+            this.ano = ano;
+        }
+    
+        public int getDia() {
+            return dia;
+        }
+    
+        public int getMes() {
+            return mes;
+        }
+    
+        public int getAno() {
+            return ano;
+        }
+    
+        public void mostraData() {
+            System.out.println(getDia() + "/" + getMes() + "/" + getAno());
+        }
+    
+    }
+
     private String nome;
     private String sobrenome;
     private Date nascimento;
@@ -13,7 +63,7 @@ public class FrequenciaCardiaca {
     }
 
     public FrequenciaCardiaca(){
-
+        this.nascimento = new Date();
     }
 
     public void setNome(String nome) {
@@ -24,8 +74,10 @@ public class FrequenciaCardiaca {
         this.sobrenome = sobrenome;
     }
 
-    public void setNascimento(Date nascimento) {
-        this.nascimento = nascimento;
+    public void setNascimento(int d, int m, int a) {
+        this.nascimento.setDia(d);
+        this.nascimento.setMes(m);
+        this.nascimento.setAno(a);
     }
 
     public String getNome() {
@@ -40,30 +92,27 @@ public class FrequenciaCardiaca {
         this.nascimento.mostraData();
     }
 
-    public int obterIdade() {
-        Scanner sc = new Scanner(System.in);
-
-        int dAt = sc.nextInt();
-        int mAt = sc.nextInt();
-        int aAt = sc.nextInt();
-
+    public int obterIdade(int dAt, int mAt, int aAt) {
 
         int idade = aAt - this.nascimento.getAno();
 
         if((mAt < this.nascimento.getMes()) || (mAt == this.nascimento.getMes() && dAt < this.nascimento.getDia()))
             idade--;
         
-        sc.close();
-        
         return idade;        
     }
 
-    public int obterFrequenciaMax() {
+    public int obterFrequenciaMax(int dAt, int mAt, int aAt) {
         int freqMax;
-        freqMax = 220-this.obterIdade();
+        freqMax = 220-this.obterIdade(dAt, mAt, aAt);
         return freqMax;
     }
-    
 
+    public double obterFrequenciaIdeal(int freqMax) {
+        double freqId;
+        freqId = freqMax*.675;
+        
+        return freqId;
+    }
 
 }
